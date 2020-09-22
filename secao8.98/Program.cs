8cb0fbe4a834665eb729ea9fa8c1374c95216058
Mikhael Meelhem <mikhaelmelhem@gmail.com>
2020-09-21 21:37:16 -0300

    Acrescentada uma mensagem de Bom Dia!

--- secao8.98/Program.cs
+++ secao8.98/Program.cs
@@ -4,6 +4,8 @@ namespace secao8._98 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom Dia!");
+
 
             Console.ReadLine();
         }
