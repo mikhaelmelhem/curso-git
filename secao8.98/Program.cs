77973900eee2ec82e5b4885157a5227e70f14eaa
Mikhael Melhem <mikhaelmelhem@gmail.com>
2020-09-23 14:03:06 -0300

    adicionada mensagem como esta voce hoje?

--- secao8.98/Program.cs
+++ secao8.98/Program.cs
@@ -5,6 +5,7 @@ namespace secao8._98 {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom Dia!");
+            Console.WriteLine("Como você esta hoje?");
 
 
             Console.ReadLine();
